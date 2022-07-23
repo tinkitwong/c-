b95d4fbc58fd625141bd456845bbf907b8a0442c
tinkitwong <tinkitwong@gmail.com>
2022-07-23 19:20:13 +0800

    feat: add variable memory

---
+++ variable-memory/variable-memory.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{   
+    // variable set aside for tuna with value of 99 
+    int tuna = 99;
+    // variable value reassigned to 76
+    tuna = 76;
+    // variable now is 76 which is the latest value
+    cout << tuna << endl;
+    return 0;
+}
