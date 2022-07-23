bd112197208f1ea6b9ed901204c958f43426c4ea
tinkitwong <tinkitwong@gmail.com>
2022-07-23 19:06:30 +0800

    feat: add basic add calculator

---
+++ basic-calculator/basic-calculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{    
+    int a;    
+    int b;
+    int sum;
+
+    cout << "Enter a number!" << endl;
+    cin >> a;
+    cout << "Enter another number!" << endl;
+    cin >> b;
+
+    sum = a + b;
+    cout << "the sum are those numbers is " << sum << endl;
+    return 0;
+}
