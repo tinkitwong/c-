310304b7fe7c97f332a02c7f3667e3b405fe4625
tinkitwong <tinkitwong@gmail.com>
2022-07-24 21:21:22 +0800

    feat: add if-else

---
+++ if-else/if-else.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 60;
+    if (age > 60) {
+        cout << "Wow you are old." << endl;
+    } else {
+        cout << "Wow you are young." << endl;
+    }
+    
+    return 0;
+}
