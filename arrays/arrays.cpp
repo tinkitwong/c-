447ccddce5e8773402a637294ce45afb5493fa78
tinkitwong <tinkitwong@gmail.com>
2022-07-27 21:31:54 +0800

    feat: add array

---
+++ arrays/arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // array of size 5 with int datatype
+    // init all values to 0
+    int bucky[5] = {0};
+
+    cout << bucky[0] << endl;
+}
