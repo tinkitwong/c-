51041154efa597ed991325e37f4bf041a448f473
tinkitwong <tinkitwong@gmail.com>
2022-08-01 21:54:44 +0800

    feat: add cd /Users/tinkitwong/Desktop/c++/files/ && g++ *.cpp -o main && /Users/tinkitwong/Desktop/c++/files/main

---
+++ files/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    // create file object
+    ofstream file;
+    // create file if it does not exist
+    file.open("tuna.text");
+    file << "I am love tuna and tuna loves me.\n";
+}
