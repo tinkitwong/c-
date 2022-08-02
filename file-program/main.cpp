719f5bf23cc1eaf61095559a7cfa5a48e7d38b47
tinkitwong <tinkitwong@gmail.com>
2022-08-02 20:50:46 +0800

    feat: add file-program-1

---
+++ file-program/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int getWhatTheyWant();
+
+// main function
+int main()
+{
+ int whatTheyWant;
+ whatTheyWant = getWhatTheyWant();
+
+ while(whatTheyWant != 4){
+    whatTheyWant = getWhatTheyWant();
+ }   
+}
+
+// getWhatTheyWant function
+int getWhatTheyWant()
+{
+    int choice;
+    cout << "1 - just for plain items" << endl;
+    cout << "2 - helpful items" << endl;
+    cout << "3 - harmful items" << endl;
+    cout << "4 - quit program" << endl;
+
+    cin >> choice;
+    return choice;
+}
